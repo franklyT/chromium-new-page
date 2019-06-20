ac417d356a1d4ac0e981ab01401b7d46b93a09c8
maldici <48483484+maldici@users.noreply.github.com>
2019-06-20 14:18:34 -0400

    catch image errors

--- src/my.js
+++ src/my.js
@@ -1,24 +1,30 @@
 // All images are royalty free via Pexels and other sources
 const imageBay = {
   bg1:
-    "url('https://images.pexels.com/photos/422218/pexels-photo-422218.jpeg')",
+    "https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg",
   bg2:
-    "url('https://images.pexels.com/photos/457447/pexels-photo-457447.jpeg')",
+    "https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg",
   bg3:
-    "url('https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg')",
+    "https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg",
   bg4:
-    "url('https://images.pexels.com/photos/877988/pexels-photo-877988.jpeg')",
+    "https://images.pexels.com/photos/877988/pexels-photo-877988.jpeg",
   bg5:
-    "url('https://images.pexels.com/photos/463732/pexels-photo-463732.jpeg')",
+    "https://images.pexels.com/photos/463732/pexels-photo-463732.jpeg",
   bg6:
-    "url('https://images.pexels.com/photos/1618676/pexels-photo-1618676.jpeg')",
+    "https://images.pexels.com/photos/1618676/pexels-photo-1618676.jpeg",
   bg7:
-    "url('https://images.pexels.com/photos/45876/beef-scotland-highland-beef-cow-45876.jpeg')",
+    "https://images.pexels.com/photos/45876/beef-scotland-highland-beef-cow-45876.jpeg",
   bg8:
-    "url('https://images.pexels.com/photos/1332026/pexels-photo-1332026.jpeg')",
+    "https://images.pexels.com/photos/1332026/pexels-photo-1332026.jpeg",
   bg9:
-    "url('https://images.pexels.com/photos/1131856/pexels-photo-1131856.jpeg')"
+    "https://images.pexels.com/photos/1131856/pexels-photo-1131856.jpeg",
+
+  get myBg() {imageBay.bg = imageBay[`bg${Math.floor(Math.random() * 9) + 1}`]},
+
+  bg: ""
 };
+
+imageBay.myBg;
 
 function gSearch() {
   window.location.href = `http://www.google.com/search?q=${
@@ -43,16 +49,16 @@ document.querySelector(".search").addEventListener("keydown", event => {
 
 function callBackground() {
   // document.querySelector('.bg').style.backgroundImage = `url('https://traina.me/images/bg${Math.floor(Math.random()*37)+1}.jpg')`;
-  document.querySelector(".bg").style.backgroundImage =
-    imageBay[`bg${Math.floor(Math.random() * 9) + 1}`];
+  document.querySelector(".bg").style.backgroundImage = "url('" + imageBay.bg; + "')"
 }
 
 callBackground();
+document.querySelector(".bg").style.display = "inline-flex";
+
 
 function getBgUrl(el) {
   var bg = "";
   bg = el.style.backgroundImage;
-  console.log(bg);
   return bg.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
 }
 
@@ -61,3 +67,22 @@ image.src = getBgUrl(document.querySelector(".bg"));
 image.onload = function() {
   document.querySelector(".bg").style.display = "inline-flex";
 };
+
+function catchFourZeroFour(url) {
+    let tester = new Image();
+    tester.addEventListener('load', imageFound);
+    tester.addEventListener('error', imageNotFound);
+    tester.src = url;
+}
+
+function imageFound() {
+}
+
+function imageNotFound() {
+    console.log(`Error: The background image requested at ${imageBay.bg} is no longer available. The program is attempting automatic recovery with another image, but this should be noted to the developer.`);
+    imageBay.myBg;
+    document.querySelector(".bg").style.backgroundImage =  "url('" + imageBay.bg; + "')";
+    catchFourZeroFour(imageBay.bg)
+}
+
+catchFourZeroFour(imageBay.bg);
