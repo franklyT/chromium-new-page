f18e2af259e86be878e339472472053b9e2f6c7e
maldici <48483484+maldici@users.noreply.github.com>
2019-06-21 16:40:56 -0400

    welcome

--- src/overlay.js
+++ src/overlay.js
@@ -1,17 +1,29 @@
 chrome.storage.sync.get("name", function(items) {
-  if (items.name === undefined) {
-    var overlay = document.createElement("div"); 
-    var overlayInput = document.createElement("input"); 
+  if (items.name === undefined || items.name === null) {
+    var overlay = document.createElement("div");
+    var overlayInput = document.createElement("input");
     overlay.innerHTML = "";
-    overlayInput.innerHtml = 'dsasddasdsa';
-    overlayInput.id = "overlayInput"
-    overlay.classList.add('name-overlay')                 
-    document.querySelector('#overlay').appendChild(overlay);    
-    document.querySelector('#overlay').appendChild(overlayInput);    
-    document.querySelector("#overlayInput").addEventListener("click", function() {
-    document.querySelector('#overlay').parentNode.removeChild(document.querySelector('#overlay'))
-    });     
-} else {
-    console.log("name found");
+    overlayInput.innerHtml = "dsasddasdsa";
+    overlayInput.id = "overlayInput";
+    overlay.classList.add("name-overlay");
+    document.querySelector("#overlay").appendChild(overlay);
+    document.querySelector("#overlay").appendChild(overlayInput);
+    document
+      .querySelector("#overlayInput")
+      .addEventListener("keydown", event => {
+        if (event.isComposing || event.keyCode === 13) {
+            
+            console.log(document.querySelector('#overlayInput'))
+
+            chrome.storage.sync.set({ name: document.querySelector('#overlayInput').value }, function() {
+            });
+            document
+            .querySelector("#overlay")
+            document.querySelector('#greeting').innerHTML = `Hi, ${document.querySelector('#overlayInput').value}`
+            .parentNode.removeChild(document.querySelector("#overlay"));
+        }
+      });
+  } else {
+      document.querySelector('#greeting').innerHTML = `Welcome back, ${items.name}.`
   }
 });
