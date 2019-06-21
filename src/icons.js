8f6e5a88f5189ee852a6f0673fea3704c661720a
maldici <48483484+maldici@users.noreply.github.com>
2019-06-21 15:06:01 -0400

    Update icons.js

--- src/icons.js
+++ src/icons.js
@@ -6,27 +6,23 @@ document.querySelectorAll(".bg-picker").forEach(elm => {
     event.target.classList.add("active");
 
     if (event.target.id === "tabby") {
-      chrome.storage.sync.set({ allData: tabbyBg, bgID: 'tabby' }, function() {
+      chrome.storage.sync.set({ allData: tabbyBg, bgID: "tabby" }, function() {
         imageBay = tabbyBg;
         imageBay.bg = myBg();
         callBackground();
         catchFourZeroFour(imageBay.bg);
-    
       });
     } else if (event.target.id === "cow") {
-      chrome.storage.sync.set({ allData: cowBg, bgID: 'cow' }, function() {   
+      chrome.storage.sync.set({ allData: cowBg, bgID: "cow" }, function() {
         imageBay = cowBg;
         imageBay.bg = myBg();
         callBackground();
         catchFourZeroFour(imageBay.bg);
-    });
+      });
     }
-
   });
 });
 
-
 chrome.storage.sync.get("bgID", function(items) {
-    document.querySelector('#'+items.bgID).classList.add('active');
+  document.querySelector("#" + items.bgID).classList.add("active");
 });
-
