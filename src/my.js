d9d06ada65cf6f8ed0c39e77a1d998f6abc2a8d3
maldici <48483484+maldici@users.noreply.github.com>
2019-06-21 00:54:33 -0400

    all

--- src/my.js
+++ src/my.js
@@ -1,17 +1,38 @@
 // All images are royalty free via Pexels and other sources
 const imageBay = {
-  bg1: "sddsdsasdasda",
-  bg2: "sdsddsdas",
-  bg3: "ddsadsadasdsa",
-  bg4: "dssdadasdas",
-  bg5: "dsasaddasdas",
-  bg6: "dssdadsadsadsa",
-  bg7: "dsasdadasdasdsa",
-  bg8: "dsdsaddasdsa",
-  bg9: "dssaddasdsa",
+  bg1: "https://images.pexels.com/photos/422202/pexels-photo-422202.jpeg",
+  bg2: "https://images.pexels.com/photos/66400/pexels-photo-66400.jpeg",
+  bg3: "https://images.pexels.com/photos/1276237/pexels-photo-1276237.jpeg",
+  bg4: "https://images.pexels.com/photos/511161/pexels-photo-511161.jpeg",
+  bg5: "https://images.pexels.com/photos/64231/cows-cow-austria-pasture-sky-64231.jpeg",
+  bg6: "https://images.pexels.com/photos/422218/pexels-photo-422218.jpeg",
+  bg7: "https://images.pexels.com/photos/735974/pexels-photo-735974.jpeg",
+  bg8: "https://images.pexels.com/photos/325257/pexels-photo-325257.jpeg",
+  bg9: "https://images.pexels.com/photos/162240/bull-calf-heifer-ko-162240.jpeg",
+  bg10: "https://images.pexels.com/photos/394887/pexels-photo-394887.jpeg",
+
+  bg11: "https://images.pexels.com/photos/422207/pexels-photo-422207.jpeg",
+
+  bg12: "https://images.pexels.com/photos/51950/calf-cow-feast-pasture-51950.jpeg",
+
+  bg13: "https://images.pexels.com/photos/119587/pexels-photo-119587.jpeg",
+
+  bg14: "https://images.pexels.com/photos/877983/pexels-photo-877983.jpeg",
+  bg15: "https://images.pexels.com/photos/144234/bull-landscape-nature-mammal-144234.jpeg",
+  bg16: "https://images.pexels.com/photos/457447/pexels-photo-457447.jpeg",
+
+  bg17: "https://images.pexels.com/photos/552766/pexels-photo-552766.jpeg",
+
+  bg18: "https://images.pexels.com/photos/420233/pexels-photo-420233.jpeg",
+
+  bg19: "https://images.pexels.com/photos/542258/pexels-photo-542258.jpeg",
+
+  bg20: "https://images.pexels.com/photos/1131856/pexels-photo-1131856.jpeg",
+
+  bg21: "https://images.pexels.com/photos/735976/pexels-photo-735976.jpeg",
 
   get myBg() {
-    imageBay.bg = imageBay[`bg${Math.floor(Math.random() * 9) + 1}`];
+    imageBay.bg = imageBay[`bg${Math.floor(Math.random() * (imageBay.bgLength.length)) + 1}`];
   },
 
   get bgLength() {
