e9dc5cb8a05d5c9232891cce1d8a5dde946cf978
maldici <48483484+maldici@users.noreply.github.com>
2019-06-21 16:55:55 -0400

    d

--- src/overlay.js
+++ src/overlay.js
@@ -3,6 +3,7 @@ chrome.storage.sync.get("name", function(items) {
     var overlay = document.createElement("div"); 
     var overlayInput = document.createElement("input"); 
     overlay.innerHTML = "";
+<<<<<<< HEAD
     overlayInput.innerHtml = 'dsasddasdsa';
     overlayInput.id = "overlayInput"
     overlay.classList.add('name-overlay')                 
@@ -13,5 +14,29 @@ chrome.storage.sync.get("name", function(items) {
     });     
 } else {
     console.log("name found");
+=======
+    overlayInput.innerHtml = "dsasddasdsa";
+    overlayInput.id = "overlayInput";
+    overlay.classList.add("name-overlay");
+    document.querySelector("#overlay").appendChild(overlay);
+    document.querySelector("#overlay").appendChild(overlayInput);
+    document
+      .querySelector("#overlayInput")
+      .addEventListener("keydown", event => {
+        if (event.isComposing || event.keyCode === 13) {
+            
+            console.log(document.querySelector('#overlayInput'))
+
+            chrome.storage.sync.set({ name: document.querySelector('#overlayInput').value }, function() {
+            });
+            document
+            .querySelector("#overlay")
+            document.querySelector('#greeting').innerHTML = `Hi, ${document.querySelector('#overlayInput').value}`
+            .parentNode.removeChild(document.querySelector("#overlay"));
+        }
+      });
+  } else {
+      document.querySelector('#greeting').innerHTML = `Welcome back, ${items.name}.`
+>>>>>>> parent of 0ea694f... v
   }
 });
