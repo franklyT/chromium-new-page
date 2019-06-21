c6c5a844f835b6776a9a592220be4052cee4a301
maldici <48483484+maldici@users.noreply.github.com>
2019-06-21 17:00:52 -0400

    d

--- src/overlay.js
+++ src/overlay.js
@@ -3,18 +3,6 @@ chrome.storage.sync.get("name", function(items) {
     var overlay = document.createElement("div"); 
     var overlayInput = document.createElement("input"); 
     overlay.innerHTML = "";
-<<<<<<< HEAD
-    overlayInput.innerHtml = 'dsasddasdsa';
-    overlayInput.id = "overlayInput"
-    overlay.classList.add('name-overlay')                 
-    document.querySelector('#overlay').appendChild(overlay);    
-    document.querySelector('#overlay').appendChild(overlayInput);    
-    document.querySelector("#overlayInput").addEventListener("click", function() {
-    document.querySelector('#overlay').parentNode.removeChild(document.querySelector('#overlay'))
-    });     
-} else {
-    console.log("name found");
-=======
     overlayInput.innerHtml = "dsasddasdsa";
     overlayInput.id = "overlayInput";
     overlay.classList.add("name-overlay");
@@ -37,6 +25,5 @@ chrome.storage.sync.get("name", function(items) {
       });
   } else {
       document.querySelector('#greeting').innerHTML = `Welcome back, ${items.name}.`
->>>>>>> parent of 0ea694f... v
   }
 });
