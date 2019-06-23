8d75712048809716b21e458bb7f5a182644b2fc2
maldici <48483484+maldici@users.noreply.github.com>
2019-06-23 01:10:44 -0400

    gear

--- src/menu-core.js
+++ src/menu-core.js
@@ -1,20 +1,37 @@
 document.body.addEventListener("click", function() {
   if (
     event.target !== document.querySelector("#settingsIcon") &&
-    !document.querySelector("#settingsParentNode").contains(event.target)
+    !document.querySelector("#settingsParentNode").contains(event.target) &&
+    !select('#settings').classList.contains('hidden')
   ) {
-    if (!document.querySelector("#settings").classList.contains("hidden")) {
+    toggleSpin();
+    document.querySelector(".settings").style.pointerEvents = "none";
+    setTimeout(function() {
+        document.querySelector(".settings").style.pointerEvents = "";
+      }, 1000);
+
       document.querySelector("#settings").classList.add("hidden");
-    }
   }
 });
 
 document.querySelector("#settingsIcon").addEventListener("click", function() {
   event.target.style.pointerEvents = "none";
-  event.target.classList.toggle("active");
-  document.querySelector(".settings-menu").classList.toggle("hidden");
+  toggleSpin();
+
+document.querySelector(".settings-menu").classList.toggle("hidden");
   setTimeout(function() {
-    document.querySelector(".settings").classList.toggle("active");
     document.querySelector(".settings").style.pointerEvents = "";
   }, 1000);
 });
+
+
+
+function toggleSpin() {
+    if (select('#settings').classList.contains('hidden')) {
+    document.querySelector(".settings").classList.remove("fSpin");
+    document.querySelector(".settings").classList.add("rSpin");
+} else {
+    document.querySelector(".settings").classList.add("fSpin");
+    document.querySelector(".settings").classList.remove("rSpin");
+}
+}
