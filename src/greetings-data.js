2f0f695568214a146207d10e5c0cf3c5393b4a0a
maldici <48483484+maldici@users.noreply.github.com>
2019-06-23 02:41:29 -0400

    Update greetings-data.js

--- src/greetings-data.js
+++ src/greetings-data.js
@@ -1,7 +1,12 @@
+name = "x";
 const greetingsList = {
-    morning: "",
-    afternoon: "",
-    night: "",
-    wildcard: "",
-    holiday: ""
+    morning: [`Good morning, ${name}!`, `Morning, ${name}!`, `Rise and shine, ${name}.`, `How did you sleep, ${name}?`,
+              `Top o' the mornin' to ya, ${name}.`, `Buenos dias, ${name}.`, `Ciao, ${name}.`, `Konichiwa, ${name}.`],
+    afternoon: [`Good afternoon, ${name}.`],
+    night: [`Good evening, ${name}.`],
+    wildcard: [`Hi there ${name}.`, `Hey, ${name}.`, `What's up, ${name}?`, `Greetings, ${name}!`, `Salute, ${name}.`, `Howdy, ${name}.`,
+                `Yo, ${name}!`, `Good day, ${name}.`, `Hello, ${name}.`, `Hi, ${name}.`, `How are things, ${name}?`,
+                `How's it going, ${name}?`, `Sup', ${name}?`, `What's new, ${name}?`, `Well, hello, ${name}.`, `Hey, what's up, ${name}?`,
+                `Hiya, ${name}.`, `How's everything, ${name}?`, `Long time no see, ${name}.`],
+    holiday: []
 }
