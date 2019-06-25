42a3340fc3a4cb48ecd192e28746d9d86fce84a5
maldici <48483484+maldici@users.noreply.github.com>
2019-06-25 15:45:55 -0400

    Update menu-history.js

--- src/menu-history.js
+++ src/menu-history.js
@@ -1,8 +1,15 @@
-select('#historyEraser').addEventListener("click", ()=> {
-    let cQuery = confirm("Are you sure you want to erase your history?\nNote: This will erase your browser's entire history permanently.");
-    if (cQuery) {
-    chrome.history.deleteAll(()=> {
-        console.log('Tabby Tab: WARNING: Your web history has been erased. If this is not expected behavior, discontinue Tabby Tab extension use.');
-      }) 
-    }
-})
+select("#historyEraser").addEventListener("click", () => {
+  let cQuery = confirm(
+    "Are you sure you want to erase your history?\nNote: This will erase your browser's entire history permanently."
+  );
+  if (cQuery) {
+    chrome.history.deleteAll(() => {
+      console.log(
+        "Tabby Tab: WARNING: Your web history has been erased. If this is not expected behavior, discontinue Tabby Tab extension use."
+      );
+    });
+    selectAll(".topsites-box").forEach(elm => {
+      elm.parentNode.removeChild(elm);
+    });
+  }
+});
