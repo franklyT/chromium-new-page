a5ea1f6a0560e57634f3640410e5a0e548ee0d35
maldici <48483484+maldici@users.noreply.github.com>
2019-06-25 01:22:06 -0400

    Update favicon-magic.js

--- src/favicon-magic.js
+++ src/favicon-magic.js
@@ -3,96 +3,96 @@
 
 //Favicon parser
 chrome.topSites.get(function(items) {
-    items.forEach( (elm) => {
-      giveUsApples(elm.url, elm.title)
-    })
+  items.forEach(elm => {
+    giveUsApples(elm.url, elm.title);
+  });
 
   function appleADay(link) {
     return new Promise(function(resolve, reject) {
-    let img = document.createElement("img");
-    img.src = `${link}apple-touch-icon.png`
-    img.onload = function() {
-      resolve(img.src);
-    }
-    
-    img.onerror = function() {
-      reject('Trying another method...');
-    }
-  });
-
-  }
-
-  async function giveUsApples(link, title=null) {
-     await appleADay(link).then( result => {
-      let div = document.createElement("div")
-      div.title = title;
-      div.classList.add('topsites-box')
       let img = document.createElement("img");
-      img.src = result;
-      div.appendChild(img);
-      select('#topSites').appendChild(div)
-      return;
-    })
-    .catch( reject => {
-      console.log(reject);
-      bruteForce(link, title);
-     } );
+      img.src = `${link}apple-touch-icon.png`;
+      img.onload = function() {
+        resolve(img.src);
+      };
+
+      img.onerror = function() {
+        reject("Trying another method...");
+      };
+    });
   }
 
-  async function bruteForce(link, title=null) {
+  async function giveUsApples(link, title = null) {
+    await appleADay(link)
+      .then(result => {
+        let div = document.createElement("div");
+        div.title = title;
+        div.classList.add("topsites-box");
+        let img = document.createElement("img");
+        img.src = result;
+        div.appendChild(img);
+        select("#topSites").appendChild(div);
+        return;
+      })
+      .catch(reject => {
+        console.log(reject);
+        bruteForce(link, title);
+      });
+  }
+
+  async function bruteForce(link, title = null) {
     let reply = await makeRequest("GET", link);
     try {
       reply = reply.match(/<link(.*?)png/gm);
       reply = reply.join("").match(/(?<=href=")(.*?)png/gm);
-  
+
       reply = reply.filter(elm => {
         return elm.split("").length < 150;
       });
     } catch {
-      let div = document.createElement("div")
+      let div = document.createElement("div");
       div.title = title;
-      div.classList.add('topsites-box')
+      div.classList.add("topsites-box");
       let img = document.createElement("img");
       img.src = "icons/domain.png";
       div.appendChild(img);
-      div.addEventListener("click", ()=> {
-        window.location.href = link});
-      select('#topSites').appendChild(div)
+      div.addEventListener("click", () => {
+        window.location.href = link;
+      });
+      select("#topSites").appendChild(div);
       return;
     }
-    
-    let div = document.createElement("div")
+
+    let div = document.createElement("div");
     div.title = title;
 
-    div.classList.add('topsites-box')
-    select('#topSites').appendChild(div)
+    div.classList.add("topsites-box");
+    select("#topSites").appendChild(div);
     let img = document.createElement("img");
-    
-  
-    if (/^https?:\/\//i.test(reply)) {
-        
-      img.src = reply[reply.length - 1];
-      div.addEventListener("click", ()=> {
-        window.location.href = link});
 
+    if (/^https?:\/\//i.test(reply)) {
+      img.src = reply[reply.length - 1];
+      div.addEventListener("click", () => {
+        window.location.href = link;
+      });
     } else {
       img.src = link + "/" + reply[reply.length - 1];
-      div.addEventListener("click", ()=> {
-        window.location.href = link});
+      div.addEventListener("click", () => {
+        window.location.href = link;
+      });
     }
-  
+
     img.onload = function() {
       if (img.width && img.height > 50) {
         div.appendChild(img);
-
-    } else {
+      } else {
         img.src = "icons/domain.png";
-        div.addEventListener("click", ()=> {
-            window.location.href = link});
+        div.addEventListener("click", () => {
+          window.location.href = link;
+        });
         div.appendChild(img);
-    }
+      }
     };
-  
+
     img.onerror = function() {
       img.src = "icons/domain.png";
       div.appendChild(img);
@@ -122,4 +122,4 @@ chrome.topSites.get(function(items) {
       xhr.send();
     });
   }
-  });
+});
