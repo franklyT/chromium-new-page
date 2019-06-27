f514ca7c7f5cc364055632dace479ae438c9cac1
maldici <48483484+maldici@users.noreply.github.com>
2019-06-27 00:21:23 -0400

    Update favicon-magic.js

--- src/favicon-magic.js
+++ src/favicon-magic.js
@@ -4,12 +4,16 @@ onDOMLoad(pullRecentSites());
 function linkLink(div, link) {
   div.addEventListener("click", () => {
     window.location.href = link;
-  })
+  });
 }
 
 function pullRecentSites() {
   chrome.history.search({ text: "", maxResults: 100 }, function(data) {
-    for (let attempt = 0, returnCount = 0; attempt < 100 && returnCount < 5; attempt++) {
+    for (
+      let attempt = 0, returnCount = 0;
+      attempt < 100 && returnCount < 5;
+      attempt++
+    ) {
       if (!data[attempt].url.includes("google")) {
         returnCount++;
         giveUsApples(data[attempt].url, data[attempt].title);
@@ -56,61 +60,62 @@ async function giveUsApples(link, title = null) {
     .catch(reject => {
       console.log(reject);
       bruteForce(link, title);
-    })
+    });
 }
 
 async function bruteForce(link, title = null) {
-  let reply = '';
-  await makeRequest("GET", link).then((response)=> {
-    reply = response.match(/<link(.*?)png/gm);
-    reply = reply.join("").match(/(?<=href=")(.*?)png/gm);
-    reply = reply.filter(elm => {
-    return elm.split("").length < 150;
-    });
-    etTuBrute(link, title, reply);
-  }
-    ).catch(()=> {
-    let div = document.createElement("div");
-    div.title = title;
-    div.classList.add("topsites-box");
+  let reply = "";
+  await makeRequest("GET", link)
+    .then(response => {
+      reply = response.match(/<link(.*?)png/gm);
+      reply = reply.join("").match(/(?<=href=")(.*?)png/gm);
+      reply = reply.filter(elm => {
+        return elm.split("").length < 150;
+      });
+      etTuBrute(link, title, reply);
+    })
+    .catch(() => {
+      let div = document.createElement("div");
+      div.title = title;
+      div.classList.add("topsites-box");
 
-    let img = document.createElement("img");
-    img.src = "icons/domain.png";
-    div.appendChild(img);
-    linkLink(div, link);
-    select("#topSites").appendChild(div);
-  })
-
-  function etTuBrute(link, title, reply) {
-  let div = document.createElement("div");
-  linkLink(div, link);
-
-  div.title = title;
-
-  div.classList.add("topsites-box");
-  select("#topSites").appendChild(div);
-  let img = document.createElement("img");
-
-  if (/^https?:\/\//i.test(reply)) {
-    img.src = reply[reply.length - 1];
-  } else {
-    img.src = link + "/" + reply[reply.length - 1];
-  }
-
-  img.onload = function() {
-    if (img.width && img.height > 50) {
-      div.appendChild(img);
-    } else {
+      let img = document.createElement("img");
       img.src = "icons/domain.png";
       div.appendChild(img);
-    }
-  };
+      linkLink(div, link);
+      select("#topSites").appendChild(div);
+    });
 
-  img.onerror = function() {
-    img.src = "icons/domain.png";
-    div.appendChild(img);
-  };
-}
+  function etTuBrute(link, title, reply) {
+    let div = document.createElement("div");
+    linkLink(div, link);
+
+    div.title = title;
+
+    div.classList.add("topsites-box");
+    select("#topSites").appendChild(div);
+    let img = document.createElement("img");
+
+    if (/^https?:\/\//i.test(reply)) {
+      img.src = reply[reply.length - 1];
+    } else {
+      img.src = link + "/" + reply[reply.length - 1];
+    }
+
+    img.onload = function() {
+      if (img.width && img.height > 50) {
+        div.appendChild(img);
+      } else {
+        img.src = "icons/domain.png";
+        div.appendChild(img);
+      }
+    };
+
+    img.onerror = function() {
+      img.src = "icons/domain.png";
+      div.appendChild(img);
+    };
+  }
 }
 
 function makeRequest(method, url) {
