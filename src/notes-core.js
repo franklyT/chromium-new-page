4455e7d16f7d848cfb8b4db0b8b24b8fb36ade2c
maldici <48483484+maldici@users.noreply.github.com>
2019-07-03 09:39:24 -0400

    notes fin

--- src/notes-core.js
+++ src/notes-core.js
@@ -7,9 +7,59 @@ onDOMLoad(
 onDOMLoad(
   select("#notes").addEventListener("click", () => {
     select("#notesTray").classList.toggle("notes__overlay--hidden");
+    if (!select("#notesTray").classList.contains("notes__overlay--hidden")) {
+      chrome.storage.sync.set(
+        {
+          notesOn: true
+        },
+        function() {}
+      );
+    } else {
+      chrome.storage.sync.set(
+        {
+          notesOn: false
+        },
+        function() {}
+      );
+    }
   })
 );
 
+onDOMLoad(
+  chrome.storage.sync.get("notesXY", function(items) {
+    select("#notesTray").style.left = items.notesXY[0];
+    select("#notesTray").style.top = items.notesXY[1];
+  })
+);
+
+onDOMLoad(
+  chrome.storage.sync.get("notesOn", function(items) {
+    if (items.notesOn) {
+      select("#notesTray").classList.toggle("notes__overlay--hidden");
+    }
+  })
+);
+
+onDOMLoad(moveInit("#notesTray", "#notesTray"));
+
+onDOMLoad(
+  select("#notes").addEventListener(
+    "contextmenu",
+    ev => {
+      ev.preventDefault();
+      const resetNotes = confirm("Reset notes position?");
+      if (resetNotes) {
+        select("#notesTray").style.left = 0;
+        select("#notesTray").style.top = 0;
+        if (select("#notesTray").classList.contains("notes__overlay--hidden")) {
+          select("#notesTray").classList.toggle("notes__overlay--hidden");
+        }
+      }
+      return false;
+    },
+    false
+  )
+);
 /*
 onDOMLoad(
   document.body.addEventListener("click", () => {
@@ -37,63 +87,72 @@ onDOMLoad(
   })
 );
 
-
 // Make the DIV element draggable:
-function makeDragable(dragHandle, dragTarget) {
-    let dragObj = null; //object to be moved
-    let xOffset = 0; //used to prevent dragged object jumping to mouse location
-    let yOffset = 0;
-  
-    document.querySelector(dragHandle).addEventListener("mousedown", startDrag, true);
-    document.querySelector(dragHandle).addEventListener("touchstart", startDrag, true);
-  
-    /*sets offset parameters and starts listening for mouse-move*/
-    function startDrag(e) {
-        if(event.target === select('#notesInput')) {
-            return;
-        }
+function moveInit(dragHandle, dragTarget) {
+  let dragObj = null; //object to be moved
+  let xOffset = 0; //used to prevent dragged object jumping to mouse location
+  let yOffset = 0;
 
-      e.preventDefault();
-      e.stopPropagation();
-      dragObj = document.querySelector(dragTarget);
-      dragObj.style.position = "absolute";
-      let rect = dragObj.getBoundingClientRect();
-  
-      if (e.type=="mousedown") {
-        xOffset = e.clientX - rect.left; //clientX and getBoundingClientRect() both use viewable area adjusted when scrolling aka 'viewport'
-        yOffset = e.clientY - rect.top;
-        window.addEventListener('mousemove', dragObject, true);
-      } else if(e.type=="touchstart") {
-        xOffset = e.targetTouches[0].clientX - rect.left;
-        yOffset = e.targetTouches[0].clientY - rect.top;
-        window.addEventListener('touchmove', dragObject, true);
-      }
+  document
+    .querySelector(dragHandle)
+    .addEventListener("mousedown", startDrag, true);
+  document
+    .querySelector(dragHandle)
+    .addEventListener("touchstart", startDrag, true);
+
+  /*sets offset parameters and starts listening for mouse-move*/
+  function startDrag(e) {
+    if (event.target === select("#notesInput")) {
+      return;
     }
-  
-    /*Drag object*/
-    function dragObject(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      if(dragObj == null) {
-        return; // if there is no object being dragged then do nothing
-      } else if(e.type=="mousemove") {
-        dragObj.style.left = e.clientX-xOffset +"px"; // adjust location of dragged object so doesn't jump to mouse position
-        dragObj.style.top = e.clientY-yOffset +"px";
-      } else if(e.type=="touchmove") {
-        dragObj.style.left = e.targetTouches[0].clientX-xOffset +"px"; // adjust location of dragged object so doesn't jump to mouse position
-        dragObj.style.top = e.targetTouches[0].clientY-yOffset +"px";
-      }
-    }
-  
-    /*End dragging*/
-    document.onmouseup = function(e) {
-      if (dragObj) {
-        dragObj = null;
-        window.removeEventListener('mousemove', dragObject, true);
-        window.removeEventListener('touchmove', dragObject, true);
-      }
+
+    e.preventDefault();
+    e.stopPropagation();
+    dragObj = document.querySelector(dragTarget);
+    dragObj.style.position = "absolute";
+    let rect = dragObj.getBoundingClientRect();
+
+    if (e.type == "mousedown") {
+      xOffset = e.clientX - rect.left; //clientX and getBoundingClientRect() both use viewable area adjusted when scrolling aka 'viewport'
+      yOffset = e.clientY - rect.top;
+      window.addEventListener("mousemove", dragObject, true);
+    } else if (e.type == "touchstart") {
+      xOffset = e.targetTouches[0].clientX - rect.left;
+      yOffset = e.targetTouches[0].clientY - rect.top;
+      window.addEventListener("touchmove", dragObject, true);
     }
   }
-  
-  makeDragable('#notesTray', '#notesTray')
-  
+
+  /*Drag object*/
+  function dragObject(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (dragObj == null) {
+      return; // if there is no object being dragged then do nothing
+    } else if (e.type == "mousemove") {
+      dragObj.style.left = e.clientX - xOffset + "px"; // adjust location of dragged object so doesn't jump to mouse position
+      dragObj.style.top = e.clientY - yOffset + "px";
+    } else if (e.type == "touchmove") {
+      dragObj.style.left = e.targetTouches[0].clientX - xOffset + "px"; // adjust location of dragged object so doesn't jump to mouse position
+      dragObj.style.top = e.targetTouches[0].clientY - yOffset + "px";
+    }
+  }
+
+  /*End dragging*/
+  document.onmouseup = function(e) {
+    if (dragObj) {
+      chrome.storage.sync.set(
+        {
+          notesXY: [
+            `${select("#notesTray").style.left}`,
+            `${select("#notesTray").style.top}`
+          ]
+        },
+        function() {}
+      );
+      dragObj = null;
+      window.removeEventListener("mousemove", dragObject, true);
+      window.removeEventListener("touchmove", dragObject, true);
+    }
+  };
+}
